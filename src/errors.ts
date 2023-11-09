b21fd1c6c94a80bc564437fbd3af024913662a24
Kazuhiro Sera <seratch@gmail.com>
2023-11-09 12:59:39 +0900

    Improve SlackAPIError/WebhookError by adding accessors

--- src/errors.ts
+++ src/errors.ts
@@ -1,11 +1,19 @@
+import { SlackAPIResponse } from "./client/response";
+
 export class SlackAPIError extends Error {
+  apiName: string;
+  error: string;
+  result: SlackAPIResponse;
   // deno-lint-ignore no-explicit-any
-  constructor(apiName: string, error: string, result: any) {
+  constructor(apiName: string, error: string, result: SlackAPIResponse) {
     const message = `Failed to call ${apiName} due to ${error}: ${JSON.stringify(
       result
     )}`;
     super(message);
     this.name = "SlackAPIError";
+    this.apiName = apiName;
+    this.error = error;
+    this.result = result;
   }
 }
 
@@ -17,9 +25,13 @@ export class TokenRotationError extends Error {
 }
 
 export class WebhookError extends Error {
+  status: number;
+  body: string;
   constructor(status: number, body: string) {
     const message = `Failed to send a message using incoming webhook/response_url (status: ${status}, body: ${body})`;
     super(message);
     this.name = "ResponseUrlError";
+    this.status = status;
+    this.body = body;
   }
 }
